f4be275fd7b211ae400319231dbe0ce6643eefa3
Dcamy <dbordwel@gmail.com>
2024-05-01 21:51:17 +0700

    Edited the h1 on the home page to say Welcome to the age of abundance

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -24,7 +24,7 @@ export default function HeroHome() {
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-            <h1 className="h1 mb-4" data-aos="fade-up">PROOF OF CONCEPT SITE NOT FUNCTIONING BY DESIGN Charting New Realms of Possibility</h1>
+            <h1 className="h1 mb-4" data-aos="fade-up">PROOF OF CONCEPT SITE NOT FUNCTIONING BY DESIGN Welcome to the age of abunance</h1>
             <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">Join the vanguard of the decentralized revolution. One node, one vote, one magnificent journey ahead.</p>
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
               <div data-aos="fade-up" data-aos-delay="400">
